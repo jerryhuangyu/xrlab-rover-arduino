b7c0ce638029cf152cbaa7c3fb1d31cea5a0a8fa
jerryhuangyu <jerry0932619628@gmail.com>
2022-04-15 10:35:11 +0800

    null

--- combine_encoder_command/combine_encoder_command.ino
+++ combine_encoder_command/combine_encoder_command.ino
@@ -4,46 +4,64 @@ unsigned long previousMillis;
 unsigned long currentMillis;
 long interval = 250;
 
-
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
   Serial.begin(115200);
   Mecaunm_Pid_Init();
   Motor_Init();
-  Encoder_Init();  
-  
+  Encoder_Init();
+
   previousMillis = millis();
 }
 
-void loop() {
-  //take command from serial
+void loop()
+{
+  // take command from serial
   taking_command();
 
-  //print encoder info
+  // print encoder info
   currentMillis = millis();
-  if((currentMillis - previousMillis) >= interval){
+  if ((currentMillis - previousMillis) >= interval)
+  {
     previousMillis = currentMillis;
     printencoder();
-    Serial.print(currentMillis);Serial.print(" : ");Serial.print(previousMillis);
+    Serial.print(currentMillis);
+    Serial.print(" : ");
+    Serial.print(previousMillis);
   }
-
 }
-void printencoder(){
-  Serial.print(",輪1:"); Serial.print(ENCODER_1.last_count);
-  Serial.print(",輪2:"); Serial.print(ENCODER_2.last_count);
-  Serial.print(",輪3:"); Serial.print(ENCODER_3.cmd_vel);
-  Serial.print(",輪4:"); Serial.print(ENCODER_4.cmd_vel);
+void printencoder()
+{
+  Serial.print(",輪1:");
+  Serial.print(ENCODER_1.last_count);
+  Serial.print(",輪2:");
+  Serial.print(ENCODER_2.last_count);
+  Serial.print(",輪3:");
+  Serial.print(ENCODER_3.cmd_vel);
+  Serial.print(",輪4:");
+  Serial.print(ENCODER_4.cmd_vel);
   Serial.println();
 }
 
-void taking_command(){
-  while(Serial.available()){
+void taking_command()
+{
+  while (Serial.available())
+  {
     char x = Serial.read();
-    switch(x){
-      case 'a': Set_Mecaunm_Position(500,50); Serial.print(" O ");  break;
-      case 's': STOP();        break;
-      case 'd': BACK();        break;
-      default: break;
+    switch (x)
+    {
+    case 'a':
+      Set_Mecaunm_Position(500, 50);
+      Serial.print(" O ");
+      break;
+    case 's':
+      STOP();
+      break;
+    case 'd':
+      BACK();
+      break;
+    default:
+      break;
     }
   }
 }
